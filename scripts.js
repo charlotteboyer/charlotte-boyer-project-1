a67629fdb676f0f79d82ba39358be0863f58e6ae
Charlotte <charlottecboyer@gmail.com>
2021-07-07 21:12:27 -0400

    adding my JS hamburger menu

---
+++ scripts.js
@@ -0,0 +1,18 @@
+//PsEuDo CoDe
+//1. display a hamburger icon that users can click in mobile view to see the menu
+//2. when icon is clicked menu appears 
+//3. when each item in the menu is clicked, link takes us to that page
+//4. when items are hovered over, color changes 
+
+const menu = document.querySelector("ul");
+
+const hamburgerButton = document.querySelector("button");
+console.log(hamburgerButton);
+
+hamburgerButton.addEventListener('click', function(){
+    if (menu.classList.contains('showMenu')) {
+        menu.classList.remove('showMenu');
+    }else {
+        menu.classList.add('showMenu');
+    }  
+})
